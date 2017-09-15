a468f097914b533ac5b21190032080030d15c004
drdr xp <drdr.xp@gmail.com>
2017-09-15 22:22:56 +0800

    watch-slave shows Errno and Error

--- watch-slave.sh
+++ watch-slave.sh
@@ -7,7 +7,7 @@ cd /usr/local/mysql-5.7.13
 
 cmds=''
 # show only field that is not empty, and gtid starts from next line
-cmds="$cmds $mysql_cmd"' "show slave status\G" | grep "Slave_IO_State\|Master_Host\|Slave_IO_Running\|Slave_SQL_Running\|Last_Errno\|Seconds_Behind_Master\|Last_IO_Errno\|Last_SQL_Errno\|Master_Server_Id\|Master_UUID\|Slave_SQL_Running_State\|Channel_Name"; '
+cmds="$cmds $mysql_cmd"' "show slave status\G" | grep "Slave_IO_State\|Master_Host\|Slave_IO_Running\|Slave_SQL_Running\|Last_Err\|Seconds_Behind_Master\|Last_IO_Err\|Last_SQL_Err\|Master_Server_Id\|Master_UUID\|Slave_SQL_Running_State\|Channel_Name"; '
 cmds="$cmds $mysql_cmd"' "select @@read_only, @@server_uuid, @@server_id\G"; '
 cmds="$cmds $mysql_cmd"' "show master status\G"; '
 cmds="$cmds $mysql_cmd"' "show processlist"; '
