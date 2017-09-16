c358749a11b3b03db15ebb6989d69939fa1cf8a6
drdr xp <drdr.xp@gmail.com>
2017-09-16 16:07:15 +0800

    watch-all.sh -i for interactive mode

--- watch-all.sh
+++ watch-all.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+interactive=0
+if [ "$1" = "-i" ]; then
+    interactive=1
+    shift
+fi
+
 while ``; do
 
     found=0
@@ -9,13 +15,23 @@ while ``; do
         port=${port#*-}
         clear
         ./watch-slave.sh $port -
-        sleep 0.3
+        if [ "$interactive" = "1" ]; then
+            echo "<<press space to continue>>.........."
+            read -n 1 c
+        else
+            sleep 0.3
+        fi
 
         found=1
     done
 
     echo "--"
     if [ "$found" = "0" ]; then
-        sleep 1
+        if [ "$interactive" = "1" ]; then
+            echo "<<press space to continue>>.........."
+            read -n 1 c
+        else
+            sleep 1
+        fi
     fi
 done
